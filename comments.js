45cfc5cd5e4aeb1711c1e113a132089bf2d829ef
JasonDavis6720 <107197332+JasonDavis6720@users.noreply.github.com>
2025-01-21 15:40:24 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -7,4 +7,14 @@ var comments = [];
 
 var server = http.createServer(function(req, res){
     if(req.url === '/'){
-        fs.readFile('./index.html', function(err,
+        fs.readFile('./index.html', function(err, data){
+            if (err) {
+                res.writeHead(500, {'Content-Type': 'text/plain'});
+                res.write('Internal Server Error');
+                res.end();
+            } else {
+                res.writeHead(200, {'Content-Type': 'text/html'});
+                res.write(data);
+                res.end();
+            }
+        });
